c06b0debc45765c7043863d4b547cfffb95d7428
Werefox <jack@wrightus.com>
2022-02-02 20:30:34 -0500

    Completed activity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,113 @@
-// TODO: Declare any global variables we need
+// declare counters
+let tailsCount = 0
+let headsCount = 0
 
+// set image paths
+const headsImage = "assets/images/penny-heads.jpg"
+const tailsImage = "assets/images/penny-tails.jpg"
 
-document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
+// get image, buttons, and orange text
+const pennyImage = document.querySelector("#pennyImage")
+const flipButton = document.querySelector("#flip")
+const clearButton = document.querySelector("#clear")
+const resultsText = document.querySelector("#message")
 
-    // TODO: Add event listener and handler for flip and clear buttons
+// get number trackers
+const headsDisplay = document.querySelector("#heads")
+const tailsDisplay = document.querySelector("#tails")
+const headsPercentDisplay = document.querySelector("#heads-percent")
+const tailsPercentDisplay = document.querySelector("#tails-percent")
 
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
+// flip button function
+flipButton.addEventListener('click', function() 
+{
+    // flip coin
+    if (Math.random() > 0.5)
+    {
+        // heads
+        headsCount++
+        pennyImage.src = headsImage
+        resultsText.textContent = "You flipped heads!"
+    }
+    else
+    {
+        // tails
+        tailsCount++
+        pennyImage.src = tailsImage
+        resultsText.textContent = "You flipped tails!"
+    }
 
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
-
-
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
-
+    updateDisplay()
 })
+
+// clear button function
+clearButton.addEventListener('click', function()
+{
+    headsCount = 0
+    tailsCount = 0
+    updateDisplay()
+})
+
+function updateDisplay() 
+{
+    // get total flips
+    let totalFlips = headsCount + tailsCount
+
+    // update counter displays
+    headsDisplay.textContent = headsCount
+    tailsDisplay.textContent = tailsCount
+
+    // make sure we aren't clearing counters or we'll divide by zero
+    if (totalFlips > 0)
+    {
+        // set percentages
+        headsPercentDisplay.textContent = Math.round((headsCount / totalFlips) * 100) + "%"
+        tailsPercentDisplay.textContent = Math.round((tailsCount / totalFlips) * 100) + "%"
+    }
+}
+
+/*
+Flipping Coin Pseudocode Steps
+Load page
+Set variables:
+Tails count
+Heads count
+Tails percentage
+Heads percentage
+Random number
+Recall doucment.queryselectors from HTML text for variables
+Number of Heads
+Percentage heads
+Number of tails
+Percentage tails
+Flip Penny button
+Clear Scoreboard button
+Coin image
+Orange You Flipped text
+Build functions to amend:
+Number of Heads
+Percentage heads
+Number of tails
+Percentage tails
+Orange text
+Build function to do head and tails percentage
+Heads % =Heads/totalFlips
+Tails %=Tails/totalFlips
+Set evenListeners:
+Flip Penny
+Clear Scoreboard
+Button Flip Penny
+Inside eventListener do If statement for when random number < 0.5 is heads
+Change image to heads
+Change orange text to You Flipped Heads!
+Insert function for heads %
+Insert functions to amend heads number and %
+Else tails
+Change image to tails
+Change orange text to You Flipped Tails!
+Insert function for tails %
+Insert function to amend tails number and %
+Button Clear Scoreboard
+Amend heads #, % and tails #, % to 0
+Amend orange text to Let's get Rolling!
+*/
